f66ecff915d6c5be2e9c6d754eef290cf70434cd
Seungpyo Suh <sspyorea@gmail.com>
2024-01-24 15:23:09 +0900

    fix: move flag to server

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const PORT = 3001;
 app.use(express.static(path.join(__dirname)));
 app.use(express.urlencoded({ extended: true }));
 
+app.post('/get-flag', (req, res) => {
+    res.json({ message: "DIMI{Every_G0t_Yummy_C00kie!}" });
+});
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
