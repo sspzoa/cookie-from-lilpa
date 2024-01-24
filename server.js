10583914f3bb2c33d77be1b7058eda5675900803
Seungpyo Suh <sspyorea@gmail.com>
2024-01-24 17:55:56 +0900

    fix: move cookie check to backend

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const path = require('path');
-
 const PORT = 3001;
 
 app.use(express.static(path.join(__dirname)));
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+function checkAllMembersYes(req) {
+    var members = ["GOSEGU", "INE", "JINGBURGER", "JURURU", "VIICHAN"];
+    return members.every(member => req.cookies[member] === "yes");
+}
 
 app.post('/get-flag', (req, res) => {
-    res.json({ message: "DIMI{Every_G0t_Yummy_C00kie!}" });
+    if (checkAllMembersYes(req)) {
+        res.status(200).send("성공!\nDIMI{Every_G0t_Yummy_C00kie!}");
+    } else {
+        res.status(400).send("Not all members have set their cookie to 'yes'");
+    }
 });
 
 app.get('/', (req, res) => {
